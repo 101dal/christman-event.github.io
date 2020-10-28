24746c4cc5035b164c425b94b75430a7e37fad78
101dal <66965262+101dal@users.noreply.github.com>
2020-10-28 17:16:47 +0100

    1

---
+++ formulaire.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Login</title>
+    </head>
+    <body>
+        <p>Enter password to login :</p>
+        <form action="secret.php" method="post">
+            <p>
+            <input type="password" name="mot_de_passe" />
+            <input type="submit" value="Valider" />
+            </p>
+        </form>
+        <p>This page is protect by a password.</p>
+    </body>
+</html>
